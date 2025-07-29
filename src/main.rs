0431d7843dd57ea97ff1d2922fa5fbb9de0c4f3b
vyPal <kubik.palacky@gmail.com>
2025-07-29 12:13:58 +0200

    Add basic routes and API docs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,74 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder, Result};
 use env_logger::Env;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
+use utoipa_actix_web::AppExt;
+use utoipa_swagger_ui::SwaggerUi;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Pings the server"),
+    ),
+)]
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Find out if a number is even", body = String),
+    ),
+    params(
+        ("number", description = "The number to compare")
+    )
+)]
+#[get("/is_even/{number}")]
+async fn is_even(path: web::Path<i64>) -> Result<String> {
+    Ok((path.into_inner() % 2 == 0).to_string())
+}
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Find out if a number is odd", body = String),
+    ),
+    params(
+        ("number", description = "The number to compare")
+    )
+)]
+#[get("/is_odd/{number}")]
+async fn is_odd(path: web::Path<i64>) -> Result<String> {
+    Ok((path.into_inner() % 2 != 0).to_string())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{App, HttpServer};
+    #[derive(OpenApi)]
+    #[openapi(
+        tags(
+            (name = "todo", description = "Todo management endpoints.")
+        ),
+    )]
+    struct ApiDoc;
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .service(hello)
+            .into_utoipa_app()
+            .openapi(ApiDoc::openapi())
+            .map(|app| app.wrap(Logger::default()))
+            .service(ping)
+            .service(is_even)
+            .service(is_odd)
+            .openapi_service(|api| {
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", api)
+            })
+            .into_app()
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
